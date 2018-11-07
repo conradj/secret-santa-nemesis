d334378cc4590fe5870336f2f44a56ddffd61d9c
conradj <conradsjackson@gmail.com>
2018-11-07 22:54:04 +0000

    async up

--- server/sendGrid.js
+++ server/sendGrid.js
@@ -4,31 +4,30 @@ const sendEmails = async messages => {
   let results = [];
   for (const message of messages) {
     const result = await sendEmail(message);
-    console.log(result);
     results.push(result);
   }
-  //await messages.forEach(async message => await sendEmail(message));
+  return results;
 };
 
-const sendEmail = msg => {
+const sendEmail = async msg => {
+  let result = { result: "begin", message: "not started" };
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  return sgMail
-    .send(msg)
-    .then(() => {
-      //Celebrate
-      return { result: "ok", message: msg };
-    })
-    .catch(error => {
-      //Log friendly error
-      console.error(error.toString());
+  try {
+    const response = await sgMail.send(msg);
+    result = { result: "ok", to: msg.to };
+  } catch (error) {
+    //Log friendly error
+    console.error(error.toString());
 
-      //Extract error msg
-      const { message, code, response } = error;
+    //Extract error msg
+    const { message, code, response } = error;
 
-      //Extract response msg
-      const { headers, body } = response;
-      return { result: "error", message: message };
-    });
+    //Extract response msg
+    const { headers, body } = response;
+    result = { result: "error", message: message };
+  }
+  console.log(result);
+  return result;
 };
 
 module.exports = sendEmails;
